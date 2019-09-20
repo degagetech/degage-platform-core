801ae7c00b615a3007dc47b3ca1b2609b5720d12
wlj <932444208@qq.com>
2019-09-20 18:34:51 +0800

    初始化

---
+++ src/Degage.Core.UnitTests/Degage.Core.UnitTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Degage.Core.UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
